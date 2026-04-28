34e145a2af5d7d8c58b909e0862373692c62f857
Landon Bird <landonous03@gmail.com>
2026-04-28 17:58:04 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        int guessCount = 0;
+
+        while (true)
+        {
+
+            guessCount++;
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"You guessed the word in {guessCount} tries!");
+
     }
 }
