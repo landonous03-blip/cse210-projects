de32fac197c792e4e5753fc8f8e77963fff1eed2
Landon Bird <landonous03@gmail.com>
2026-04-30 20:46:50 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        Console.Write("Enter number: ");
+        
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != 0)
+        { 
+            numbers.Add(guess);
+
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            Console.Write("Enter number: ");
+            
+            guess = int.Parse(Console.ReadLine());
+
+        }
+
+        int sum = 0;
+
+        int largestNumber = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+
+            if (number >= largestNumber)
+            {
+                largestNumber = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
+
     }
 }
